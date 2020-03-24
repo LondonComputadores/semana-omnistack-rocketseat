69f32300bb919b5f7720cc129c8ed4a005bd0fbd
LondonComputadores <london.computadores@gmail.com>
2020-03-24 03:14:32 -0400

    node-modules bug fixes on wsl ubuntu win10

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,7 +1,16 @@
 const axios = require('axios');
-const Dev = require('../models/Dev'); 
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+// Controller possui 5 funções, não esquecer: index, show, store, update, destroy;
 
 module.exports = {
+  async index(request, response) {
+    const devs = await Dev.find();
+
+    return response.json(devs);
+  },
+
   async store(request, response) {
     const { github_username, techs, latitude, longitude } = request.body;
 
@@ -12,7 +21,7 @@ module.exports = {
      
       const { name = login, avatar_url, bio } = apiResponse.data;
     
-      const techsArray = techs.split(',').map(tech => tech.trim());
+      const techsArray = parseStringAsArray(techs);
     
       const location = {
         type: 'Point',
@@ -26,9 +35,9 @@ module.exports = {
         bio,
         techs: techsArray,
         location,
-      })
+      });
     }
-            
-      return response.json(dev);
+
+    return response.json(dev);
   }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -12,18 +12,5 @@ mongoose.connect('mongodb+srv://admin_user:admin2018@cluster0-lteks.mongodb.net/
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333); 
-
-//Métodos HTTP: GET, POST, PUT, DELETE;
-//Tipos de Parâmetros= 
-//1- Query Params: request.query (filtros, ordenação, paginação,...);
-//2- Route Params: request.params (identificar um recurso na alteração ou remoção);
-//3- Body: request.body (Dados para criação ou alteração de um registro);
-//MongoDB Atlas (Não-Relacional Online/Cloud DB)
-
-/*app.get('/users', (request, response) => {
-    console.log(request.query);
-    return response.json({ message: 'Hello OmniStack' });
-}); 
-*/
+app.listen(3333);
 
